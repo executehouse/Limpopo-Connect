f741ffbcf4fbfb3d06a544ee2feb0faa8fbf41f4
Tshikwetamakole <Tshikwetamakole@users.noreply.github.com>
2025-09-26 12:19:02 +0000

    deploy: d4276824e13d132ab1c7fb3cd9a09a9ad860e38c

---
+++ sw.js
@@ -0,0 +1,49 @@
+const CACHE_NAME = 'limpopo-connect-v1';
+const urlsToCache = [
+  '/',
+  '/business-directory',
+  '/events',
+  '/marketplace',
+  '/tourism',
+  '/news',
+  '/connections',
+  '/manifest.json',
+  '/vite.svg'
+];
+
+// Install service worker
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => {
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+// Fetch event - serve from cache when offline
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => {
+        // Return cached version or fetch from network
+        return response || fetch(event.request);
+      }
+    )
+  );
+});
+
+// Activate service worker
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
